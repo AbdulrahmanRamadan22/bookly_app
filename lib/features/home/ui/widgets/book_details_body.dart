6300805378f5368abfdfed0aa8926437a46c298d
abdulrahman <abdulrahmanramadan22@gmail.com>
2024-01-29 17:04:10 +0200

    added BookDetailsSection

--- lib/features/home/ui/widgets/book_details_body.dart
+++ lib/features/home/ui/widgets/book_details_body.dart
@@ -1,8 +1,6 @@
 import 'package:bookly_app/core/helpers/spacing.dart';
 import 'package:bookly_app/core/theming/styles.dart';
-import 'package:bookly_app/core/widgets/custom_text_button.dart';
-import 'package:bookly_app/features/home/ui/widgets/build_book_item_view.dart';
-import 'package:bookly_app/features/home/ui/widgets/book_rating.dart';
+import 'package:bookly_app/features/home/ui/widgets/book_details_section.dart';
 import 'package:bookly_app/features/home/ui/widgets/build_book_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -21,51 +19,7 @@ class BookDetailsBody extends StatelessWidget {
             child: Column(
               children: [
                 verticalSpace(10),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 90.w),
-                  child: const BuildBookItemView(),
-                ),
-                verticalSpace(25),
-                Text(
-                  "The Jungle Book",
-                  style: TextStyles.font24Regular,
-                ),
-                verticalSpace(3),
-                Text(
-                  " Rudyard Kipling",
-                  style: TextStyles.font16Medium.copyWith(color: Colors.grey),
-                ),
-                verticalSpace(14),
-                const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-                verticalSpace(30),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.w),
-                  child: Row(
-                    children: [
-                      Expanded(
-                          child: CustomTextButton(
-                        backgroundColor: Colors.white,
-                        text: "19.99€",
-                        fontSize: 18.sp,
-                        textColor: Colors.black,
-                        borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(16),
-                          topLeft: Radius.circular(16),
-                        ),
-                      )),
-                      const Expanded(
-                          child: CustomTextButton(
-                        backgroundColor: Color(0xffEF8262),
-                        text: "Free preview",
-                        textColor: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(16),
-                          topRight: Radius.circular(16),
-                        ),
-                      )),
-                    ],
-                  ),
-                ),
+                const BookDetailsSection(),
                 Expanded(child: verticalSpace(50)),
                 Align(
                     alignment: Alignment.centerLeft,
@@ -84,3 +38,4 @@ class BookDetailsBody extends StatelessWidget {
     );
   }
 }
+
